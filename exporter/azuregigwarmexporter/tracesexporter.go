8110e1d82b7d053bbf5f71a37f3f9247b5258850
Ganga Mahesh Siddem <gangams@microsoft.com>
2025-11-06 08:20:16 -0800

    fix dimension names to avoid conflicts

--- exporter/azuregigwarmexporter/tracesexporter.go
+++ exporter/azuregigwarmexporter/tracesexporter.go
@@ -236,7 +236,7 @@ func (e *tracesExporter) getCommonAttributes() []attribute.KeyValue {
 		attribute.String("gigwarm_account", e.cfg.Account),
 		attribute.String("gigwarm_namespace", e.cfg.Namespace),
 		attribute.String("gigwarm_region", e.cfg.Region),
-		attribute.Int("gigwarm_config_major_version", e.cfg.ConfigMajorVersion),
+		attribute.Uint32("gigwarm_config_major_version", e.cfg.ConfigMajorVersion),
 	}
 }
 
